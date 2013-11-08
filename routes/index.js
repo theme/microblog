716cf27a05f7777f30e1e28574c5e43a51d7c7e9
theme <xenotheme@gmail.com>
2013-11-08 12:50:40 +0800

    add post function

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,33 @@
 var crypto = require('crypto');
 var User = require('../models/user.js');
+var Post = require('../models/post.js');
+
+function checkLogin(req, res, next){
+    if( !req.session.user){
+        req.flash('error','未登入');
+        return res.redirect('/login');
+    }
+    next();
+}
+
+function checkNotLogin(req, res, next){
+    if( req.session.user){
+        req.flash('error','已登入');
+        return res.redirect('/');
+    }
+    next();
+}
 
 module.exports = function(app){
     app.get('/', function(req, res){
-        res.render('index', {
-            title: '首页'
+        Post.get(null, function(err, posts) {
+            if (err) {
+                posts = [];
+            }
+            res.render('index', {
+                title: '首页',
+                posts:posts,
+            });
         });
     });
 
@@ -88,21 +111,42 @@ module.exports = function(app){
         req.flash('success','已登出');
         res.redirect('/');
     });
+
+    app.post('/post', checkLogin);
+    app.post('/post', function(req, res) {
+        var currentUser = req.session.user;
+        var post = new Post (currentUser.name, req.body.post);
+        post.save(function(err) {
+            if (err) {
+                req.flash('error', err );
+                return res.redirect('/');
+            }
+            req.flash('success', '发表成功');
+            res.redirect('/u/' + currentUser.name );
+        });
+    });
+
+    app.get('/u/:user', function(req, res) {
+        console.log('routes/index %s', req.url);
+        User.get(req.params.user, function(err, user) {
+            if (!user) {
+                req.flash('error','用户不存在');
+                return res.redirect('/');
+            }
+
+            Post.get(user.name, function(err, posts) {
+                if (err) {
+                    req.flash('error',err);
+                    return res.redirect('/');
+                }
+
+                res.render('user',{
+                    user: user,
+                    title: user.name,
+                    posts: posts
+                });
+            });
+        });
+    });
 };
-
-function checkLogin(req, res, next){
-    if( !req.session.user){
-        req.flash('error','未登入');
-        return res.redirect('/login');
-    }
-    next();
-}
-
-function checkNotLogin(req, res, next){
-    if( req.session.user){
-        req.flash('error','已登入');
-        return res.redirect('/');
-    }
-    next();
-}
 
